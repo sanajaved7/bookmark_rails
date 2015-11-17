74519e2a84f4a3f03627109dd9bf9422ae3d7c20
Sana Javed <sanajaved7@gmail.com>
2015-11-16 22:09:53 -0500

    Database migration to add user_id to Bookmark model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116220901) do
+ActiveRecord::Schema.define(version: 20151117030653) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
