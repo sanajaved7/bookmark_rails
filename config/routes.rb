82bd42d430106901d54947a7cb75f473db87650a
Sana Javed <sanajaved7@gmail.com>
2015-11-18 14:41:24 -0500

    Fixed error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,6 +6,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'bookmarks#index'
+
+  devise_for :user
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
